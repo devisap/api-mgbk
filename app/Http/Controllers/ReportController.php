125b7ea16e75d566d305f6a3f77bda4f6e79fe01
Ilham Sagita Putra <50571957+devisap@users.noreply.github.com>
2021-02-19 06:59:57 +0700

    change status message report by date 2

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -80,7 +80,7 @@ class ReportController extends Controller
         }
 
         $reports = DB::table('v_laporan')->where('id_user', $req['id_user'])->where('id_sekolah', $req['id_sekolah'])->where('tgl_transaksi', $req['tgl_transaksi'])->get();
-        if($reports == != null){
+        if($reports != null){
             return response()->json(['status' => true, 'message' => 'Data berhasil ditemukan', 'data' => $reports]);    
         }else{
             return response()->json(['status' => false, 'message' => 'Data tidak ditemukan', 'data' => []);    
